962deacd6dc227ae4d104cbd4968348438b17600
RadBia <rad.bialkowski@gmail.com>
2024-06-04 18:31:59 +0200

    Initial commit

---
+++ src/test/java/com/myapp/jobportal/JobportalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myapp.jobportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobportalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

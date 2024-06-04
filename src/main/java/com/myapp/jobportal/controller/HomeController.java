088c384433d65e9523eb5352f7e1e0b5ceaac32c
RadBia <rad.bialkowski@gmail.com>
2024-06-04 19:00:49 +0200

    Add home page

---
+++ src/main/java/com/myapp/jobportal/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.myapp.jobportal.controller;
+
+import jakarta.servlet.http.PushBuilder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
+}

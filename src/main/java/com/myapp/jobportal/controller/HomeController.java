c2bf69636f62e00b60d2744329addb86f1f1e3ec
RadBia <rad.bialkowski@gmail.com>
2024-06-05 11:26:16 +0200

    Create Users and UsersType entities

--- src/main/java/com/myapp/jobportal/controller/HomeController.java
+++ src/main/java/com/myapp/jobportal/controller/HomeController.java
@@ -1,6 +1,5 @@
 package com.myapp.jobportal.controller;
 
-import jakarta.servlet.http.PushBuilder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 

8383383ef7aa8316b5eb5f5d9f8dc8786fd9f051
RadBia <rad.bialkowski@gmail.com>
2024-06-13 11:29:10 +0200

    Add password encoder to UserService

--- src/main/java/com/myapp/jobportal/services/UserService.java
+++ src/main/java/com/myapp/jobportal/services/UserService.java
@@ -6,6 +6,7 @@ import com.myapp.jobportal.entity.Users;
 import com.myapp.jobportal.repository.JobSeekerProfileRepository;
 import com.myapp.jobportal.repository.RecruiterProfileRepository;
 import com.myapp.jobportal.repository.UsersRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
@@ -16,18 +17,21 @@ public class UserService {
     private final UsersRepository usersRepository;
     private final RecruiterProfileRepository recruiterProfileRepository;
     private final JobSeekerProfileRepository jobSeekerProfileRepository;
+    private final PasswordEncoder passwordEncoder;
 
 
     public UserService(UsersRepository usersRepository, RecruiterProfileRepository recruiterProfileRepository,
-                       JobSeekerProfileRepository jobSeekerProfileRepository) {
+                       JobSeekerProfileRepository jobSeekerProfileRepository, PasswordEncoder passwordEncoder) {
         this.usersRepository = usersRepository;
         this.recruiterProfileRepository = recruiterProfileRepository;
         this.jobSeekerProfileRepository = jobSeekerProfileRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public Users addNew(Users users) {
         users.setActive(true);
         users.setRegistrationDate(new Date(System.currentTimeMillis()));
+        users.setPassword(passwordEncoder.encode(users.getPassword()));
         int userTypeId = users.getUserTypeId().getUserTypeId();
         Users savedUser = usersRepository.save(users);
         if(userTypeId == 1) {

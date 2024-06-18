4ad9f5393d8850f8be7a7475e239e48f3cc8d487
RadBia <rad.bialkowski@gmail.com>
2024-06-18 18:34:40 +0200

    Add JobPostActivityRepository and JobPostActivityService

---
+++ src/main/java/com/myapp/jobportal/repository/JobPostActivityRepository.java
@@ -0,0 +1,8 @@
+package com.myapp.jobportal.repository;
+
+import com.myapp.jobportal.entity.JobPostActivity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface JobPostActivityRepository extends JpaRepository<JobPostActivity, Integer> {
+
+}

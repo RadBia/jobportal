671b724a6edd6b033c47b5498ed3ee25958ba899
RadBia <rad.bialkowski@gmail.com>
2024-06-23 14:23:05 +0200

    Edit JobSeekerApplyController to edit jobs

--- src/main/java/com/myapp/jobportal/controller/JobSeekerApplyController.java
+++ src/main/java/com/myapp/jobportal/controller/JobSeekerApplyController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class JobSeekerApplyController {
@@ -30,5 +31,13 @@ public class JobSeekerApplyController {
         model.addAttribute("user", userService.getCurrentUserProfile());
         return "job-details";
 
+    }
+
+    @PostMapping("dashboard/edit/{id}")
+    public String editJob(@PathVariable("id") int id, Model model) {
+        JobPostActivity jobPostActivity = jobPostActivityService.getOne(id);
+        model.addAttribute("jobPostActivity", jobPostActivity);
+        model.addAttribute("user", userService.getCurrentUserProfile());
+        return "add-jobs";
     }
 }
